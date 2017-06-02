f4a7976a6b2f68a17abb58c44b3c51ba330b29f6
DaavidBaal <david.bartolome.alonso@tajamar365.com>
2017-06-02 21:31:00 +0200

    casicasi

--- Formulario/script/clases.js
+++ Formulario/script/clases.js
@@ -75,65 +75,102 @@ else
 /*********************************************
  *Funcion de recogida y presentación de datos
  *********************************************/
- /
-            Function.prototype.bind = function () {
-                var fn = this, args = Array.prototype.slice.call(arguments),
-                    object = args.shift();
-                return function () {
-                    return fn.apply(object,
-                      args.concat(Array.prototype.slice.call(arguments)));
-                };
-            };
-        }
+ 
+ class Formulario{
+	 constructor(){
 		
-	function main() {
-            let oFormulario = {
-                oEvento: {},
-                oDatos: {},
-                oLista: document.getElementById("listaDatos"), // referencia del párrafo donde escribir
-                recogeDatos: function (oE) {
-                    // manejadora del evento submit del formulario:
-                    // disparada sólo despues de la validación HTML5
-                    this.oEvento = oE || window.evnet;
-                    var inputs = document.querySelectorAll("input[type='text']")
-                    for (var i = 0; i < inputs.length; i++) {
-                        this.oDatos[inputs[i].name] = inputs[i].value;
-                    }
-                    this.oDatos.Comentarios = document.getElementById("Comentarios").value;
-                    this.escribeDatos();
-                }, //Fin del método recogeDatos
-                escribeDatos: function () {
-                    //anula el comportamiento por defecto de submit
-                    //incluida la validacion de los requireds
-                    //que ya se ha realizado para poder llegar aqui
-                    this.oEvento.preventDefault();
-                    //ocultar formulario
-                    document.getElementById("formulario").classList.add("oculto")
-                    //mostrar bloque div para el resultado
-                    document.getElementById("resultado").classList.remove("oculto");
-                    // limpiar el nodo <ul> donde se presentarán los datos
-                    this.oLista.innerHTML = "";
-                    //incorporamos a la lista cada elemento del objeto		
-                    for (var i in this.oDatos) {
-                        this.oLista.innerHTML += "<li>" + i + ": <strong>" +
-                            this.oDatos[i] + "</strong></li>";
-                    };
-                }, //Fin del método escribedatos	
-            }; // Fin del objeto oFormulario
-            
-            //document.getElementById("submit").addEventListener("click",
-            //   oFormulario.recogeDatos.bind(oFormulario));
+			this.oEvento = {}
+			this.oName = document.querySelector("#fname"),
+			this.oApell = document.querySelector("#apell"),
+			this.oAddress = document.querySelector("#address"),
+			this.oCity = document.querySelector("#city"),
+			this.oComent = document.querySelector("#coment"),
+			this.aJoomla = document.querySelectorAll("[name = pregunta]")
+			this.oCond = document.querySelector("#condiciones"),
+			this.oCond1 = document.querySelector("#condiciones_1"),
+			this.oOpciones = document.querySelector("#opciones"),
+			this.oParrafo = document.querySelector("#parrafo_1"), // referencia del párrafo donde escribir
+			this.aSalida = []
+			this.oName.focus();	
+		}
 
-            // En lugar de responder al evento click del boton enviar
-            // Respondemos al evento submit del formulario, 
-            // desencadenado por el boton pero POSTERIOR
-            // al proceso de validación HTML
+		getTextos () {
+			this.aSalida = [
+				{etiqueta : "Nombre", valor : this.oName.value},
+				{etiqueta : "Apellidos", valor : this.oApell.value},
+				{etiqueta : "Dirección", valor : this.oAddress.value},
+				{etiqueta : "Ciudad", valor : this.oCity.value},
+				{etiqueta : "Comentarios", valor : this.oComent.value}
+			]
+		}; //Fin de la función getTextos 
 
-            document.getElementById('formulario').addEventListener("submit",oFormulario.recogeDatos.bind(oFormulario))
+		getRRadiobuttons () {				
+			for(let i=0; i < this.aJoomla.length; i++) {
+				if (this.aJoomla[i].checked) {
+					this.aSalida[this.aSalida.length] = {
+						etiqueta : "Preparado para dar Joomla!", valor : this.aJoomla[i].value.toUpperCase()}
+				};
+			};
+		}; //Fin de la función getRRadiobuttons
+
+		getCheckbox () {
+			if (this.oCond.checked) {
+				this.aSalida[this.aSalida.length] = {etiqueta : "Condiciones leidas!", valor : "SI"}
+			} 
+			else{
+				this.aSalida[this.aSalida.length] =  {etiqueta : "Condiciones leidas!", valor : "NO"};		
+			};	
+			if (this.oCond1.checked) {
+				this.aSalida[this.aSalida.length] = {etiqueta : "Condiciones leidas 2!", valor : "SI"}
+			} 
+			else{
+				this.aSalida[this.aSalida.length] =  {etiqueta : "Condiciones leidas 2!", valor : "NO"}	
+			};
+		}; //Fin de la función getCheckbox
+
+		getSelectOptions () {
+			let oOpcion = this.oOpciones.options[this.oOpciones.selectedIndex];
+			var textoSeleccionado = oOpcion.text; // Segundo valor
+			var valorSeleccionado = oOpcion.value; // 2
+			this.aSalida[this.aSalida.length] = {etiqueta : "Nivel de JavaScript", valor : textoSeleccionado} 
+		}; //Fin de la función getSelectOptions
 
 
-                
-        } // Fin de la función main()
+		recogeDatos (oE) {
+			this.oEvento = oE;
+			// Llamada a las funciones get de cada conjunto de controles
+			this.getTextos();
+			this.getRRadiobuttons();
+			this.getCheckbox();
+			this.getSelectOptions();
+			//llamada a la función que procesara el array presentandolo en pantalla
+			this.escribeDatos ();
+		} // Fin de la funcion recogeDatos
 
-	    document.addEventListener("DOMContentLoaded", main, false);
+		escribeDatos () {
+			//anula el comportamiento por defecto de submit
+			//incluida la validacion de los requireds
+			//que ya se ha realizado para poder llegar aqui
+			this.oEvento.preventDefault();
+			//ocultar formulario
+			document.querySelector("#form_1").className = "oculto";
+			//mostrar bloque div para el resultado
+			document.querySelector("#resultado").className = "bloque";
+			//incorporamos al parrafo cada línea del array		
+			for (var i = 0; i < this.aSalida.length; i++) {
+				this.oParrafo.innerHTML 		  
+					+= `<strong> ${this.aSalida[i].etiqueta} : </strong>
+					${this.aSalida[i].valor}</br>
+			`};
+		} // Fin de la funcion escribeDatos
+	} // Fin de la clase formulario
+
 	
+	document.addEventListener("DOMContentLoaded", function () {
+			oFormulario = new Formulario
+			document.querySelector("#submit").addEventListener("click", oFormulario.recogeDatos.bind(oFormulario))
+		}, false);	
+
+	 }
+ }
+         
